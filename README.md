1a25edad5805f2bbff85716b72194502ac98c2d1
Nabil Darwich <ndarwich@gmu.edu>
2018-05-19 14:38:11 -0400

    init git repo, init npm repo, init gulp, setup all starting files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Nabil Darwich Personal website
+
+## Description
+
+This is the personal website of Nabil Darwich.
+
+## Setup
+
+```
+npm install
+npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  return gulp.src(['public_html/scss/*.scss'])
+      .pipe(sass())
+      .pipe(gulp.dest("public_html/css")) //the css desctination is in the css folder
+      .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function() {
+  browserSync.init({
+      server: "./public_html"
+  });
+  gulp.watch(['public_html/scss/*.scss'], ['sass']); //treat the scss file as gulp's sass
+  gulp.watch("public_html/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']); //default gulp is gulp serve
