29803608154fbe4da69083ee58270996bf17ea1f
Nabil Darwich <ndarwich@gmu.edu>
2018-05-20 18:55:29 -0400

    allow active page to be have purple bg. create skeleton js files for each page.

---
+++ public_html/js/index.js
@@ -0,0 +1,3 @@
+$(window).on("load", function() {
+  loadPage(0);
+});
--- public_html/js/nav.js
+++ public_html/js/nav.js
@@ -1,9 +1,19 @@
 
 $(function(){
   $('#navigation').load('navigation.html');
-})
+});
 
 
-$(document).ready(function() {
+$(window).on( "load", function() {
     $('#page-body').addClass('load-page');
 });
+
+function loadPage(p) {
+  //get rid of active class for all nav bar items
+  /*
+  for (let i = 0; i < 4; i++) {
+    $('#nav' + i).removeClass('active');
+  }*/
+  //load new active page
+  $('#nav' + p).addClass('active');
+}
