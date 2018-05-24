4160b1e134bfa798e4a1fe6fe12bedeaf0478a32
Nabil Darwich <ndarwich@gmu.edu>
2018-05-24 02:19:34 -0400

    improve contact form, make capcha required

--- public_html/js/contact.js
+++ public_html/js/contact.js
@@ -1,3 +1,22 @@
 $(window).on("load", function() {
   loadPage(3);
 });
+
+var capchaValid = false;
+
+function validateCapcha() {
+  capchaValid = true;
+}
+
+function expireCapcha() {
+  capchaValid = false;
+}
+
+function checkCapcha(e) {
+  e.preventDefault();
+  if (capchaValid) {
+    return true;
+  }
+  alert("Please prove you are human");
+  return false;
+}
