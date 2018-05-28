138556db382f2fa9c6389b717d4369cdccd8ff29
Nabil Darwich <ndarwich@gmu.edu>
2018-05-28 18:15:37 -0400

    include new DormBuddy photos. adjust css for contact page. attempt executing php POST req.

--- public_html/public/js/contact.js
+++ public_html/public/js/contact.js
@@ -13,36 +13,46 @@ function expireCapcha() {
 }
 
 function checkCapcha(e) {
-  e.preventDefault();
   if (capchaValid) {
-    return formSubmitted();
+    return checkForm();
   }
   alert("Please prove you are human");
   return false;
 }
 
-function formSubmitted()
+function checkForm()
 {
-	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	var valid = true;
-	if (! re.test(document.getElementById("email").value)){
-		document.getElementById("emaill").innerHTML = "Valid emails only! Enter a valid email pleaseeeeee:";
-		valid=false;
-	}
-	if (document.getElementById("name").value == ""){
-		document.getElementById("namee").innerHTML = "Okay, stranger, enter some name:";
-		valid=false;
-	}
-	if (document.getElementById("subject").value == ""){
-		document.getElementById("subjectt").innerHTML = "Telling me what you will talk about will be helpful:";
-		valid=false;
-	}
-	if (document.getElementById("message").value == ""){
-		document.getElementById("messagee").innerHTML = "I think you forgot something:";
-		valid=false;
-	}
-	if (! valid){
-		document.getElementById("submitt").value = "Try Again";
-	}
+  var valid = true;
+  try {
+    var re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  	if (! re.test(document.getElementById("emaill").value)){
+  		$("#emaill").val("Email?");
+      $("#emaill").addClass("invalidInput");
+  		valid=false;
+  	}
+  	if (document.getElementById("namee").value === ""){
+  		$("#namee").val("Anonymous?");
+      $("#namee").addClass("invalidInput");
+  		valid=false;
+  	}
+  	if (document.getElementById("subjectt").value === ""){
+  		$("#namee").val("Subject?");
+      $("#namee").addClass("invalidInput");
+  		valid=false;
+  	}
+  	if (document.getElementById("messagee").value === ""){
+  		$("#messagee").val("Message Body?");
+      $("#messagee").addClass("invalidInput");
+  		valid=false;
+  	}
+  	if (! valid){
+  		$("#submit-btn").val("Try Again");
+      $("#submit-btn").css("background-color", "red");
+  	}
+  } catch (e) {
+    valid = false;
+    console.error(e);
+  }
 	return valid;
 }
--- public_html/server.js
+++ public_html/server.js
@@ -1,7 +1,13 @@
-var express = require("express");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const bodyParser = require("body-parser");
+const querystring = require("querystring");
+const request = require("request");
 var app = express();
-var path = require("path");
 var router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res){
   res.setHeader("Content-Type", "text/html");
@@ -48,12 +54,43 @@ app.get("/pages/:pageName", function(req, res){
   res.sendFile("/public/pages/" + req.params.pageName, {root: __dirname });
 });
 
+app.post("/contact", function(req, res){
+  let postData = req.body;
+  var querystring = require("querystring");
+  var qs = querystring.stringify(postData);
+  var qslength = qs.length;
+  var options = {
+      hostname: "nabildarwich.com",
+      port: 80,
+      path: "/public/php/sendMail.php",
+      method: "POST",
+      headers:{
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Content-Length": qslength
+      }
+};
 
+var buffer = "";
+var req = http.request(options, function(res) {
+    res.on("data", function (chunk) {
+       buffer+=chunk;
+    });
+    res.on("end", function() {
+        console.log(buffer);
+    });
+});
+
+req.write(qs);
+req.end();
+});
+
+/*
 app.get("*", function(req, res){
   res.status(404);
   res.setHeader("Content-Type", "text/html");
   res.sendFile("/error.html", {root: __dirname });
 });
+*/
 
 //port for express server
 app.listen(3002, function () {
