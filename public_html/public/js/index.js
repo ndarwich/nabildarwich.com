e90d6abc028b3a4976b163ae123005b72765d11d
Nabil Darwich <ndarwich@gmu.edu>
2018-06-06 04:39:29 -0400

    track users with cookie, correctly update total visitors and current visitor number

--- public_html/public/js/index.js
+++ public_html/public/js/index.js
@@ -1,4 +1,15 @@
 $(window).on("load", function() {
   loadNavigation(0);
-  document.cookie = "magicCookie=1";
+  let storedCookie = document.cookie;
+  let myNumber = -1;
+  $.get("/numVisitors", function(data, status){
+    myNumber = data;
+    if (storedCookie == null || storedCookie === "") {
+      $("#myNumberSpan").text(myNumber);
+      document.cookie = "magicCookie=" + myNumber;
+    } else {
+      $("#myNumberSpan").text(document.cookie.substr(12));
+    }
+    $("#numVisitorsSpan").text(myNumber);
+  });
 });
--- public_html/routes/index.js
+++ public_html/routes/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-
+let numVisits = 1;
 
 //index router
 router.get("/", (req, res) => {
-    let storedCookie = req.headers.cookie;
-    if (storedCookie == null) {
-      console.info("NEW VISITOR");
-    }
-    res.setHeader("Content-Type", "text/html");
-    res.sendFile("/index.html", { root: __dirname + "/../public" });
+  let storedCookie = req.headers.cookie;
+  if (storedCookie == null) {
+    numVisits++;
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.sendFile("/index.html", { root: __dirname + "/../public" });
+});
+
+router.get("/numVisitors", (req, res) => {
+  console.info("visitors request " + numVisits);
+  res.status(200).send("" + numVisits);
 });
 
 module.exports = router;
