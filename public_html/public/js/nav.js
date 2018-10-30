58695218e3935f86a54b7556cfe1932f02556b73
Nabil Darwich <ndarwich@gmu.edu>
2018-10-30 03:06:16 -0400

    allow changing images using arrow keys. numerous bug fixes. reindexing and renaming of most photos. convert jpegs to pngs. upload new home image

--- public_html/public/js/nav.js
+++ public_html/public/js/nav.js
@@ -5,11 +5,36 @@ loadNavigation = (p) => {
   });
 }
 
+baseUrl = "";
+numRegex = /\d+/;
+insideImage = true;
+currentlyDisplaying = -1;
+maxIndex = -1;
 $(window).on("load", function() {
     $("#page-body").addClass("load-page");
     $("body").on("click", ".displayable-photo", (e) => {
       console.info(e.target.src);
       loadImage(e.target.src);
+    });
+    $(document).keypress(function(e){
+      if (insideImage == true) {
+        console.info(currentlyDisplaying);
+        if (e.keyCode==37) {
+          currentlyDisplaying -= 1;
+          if (currentlyDisplaying < 0){
+            currentlyDisplaying = maxIndex; //wrap
+          }
+          imgUrl = baseUrl + currentlyDisplaying + ".png";
+          loadImage(imgUrl);
+        } else if (e.keyCode == 39) {
+          currentlyDisplaying += 1;
+          if (currentlyDisplaying > maxIndex){
+            currentlyDisplaying = 0; //wrap
+          }
+          imgUrl = baseUrl + currentlyDisplaying + ".png";
+          loadImage(imgUrl);
+        }
+      }
     });
     $("body").on("click", "#page-overlay", () => {
       exitImage();
@@ -22,12 +47,20 @@ $(window).on("load", function() {
 let loadImage = (imgUrl) => {
   $("#page-overlay").load("/components/image-page.html", () => {
     $("#page-overlay").addClass("load-overlay");
-    console.info(imgUrl);
     $("#main-photo-img").attr("src", imgUrl);
+    if (insideImage == false) {
+      baseUrl = imgUrl.substring(0, imgUrl.lastIndexOf("-") + 1);
+      ending = imgUrl.substring(imgUrl.lastIndexOf("-") + 1);
+      currentlyDisplaying = parseInt(ending.match(numRegex));
+      maxIndex = $(".displayable-photo").length - 1;
+      insideImage = true;
+    }
   });
 }
 
 let exitImage = () => {
   $("#page-overlay").empty();
   $("#page-overlay").removeClass("load-overlay");
+  insideImage = false;
+  currentlyDisplaying = -1;
 }
