c047637051f9b454ca362886f9aa45b398130e38
Nabil Darwich <ndarwich@gmu.edu>
2019-04-27 22:07:53 -0400

    fill text book inventory. Nabil's books now sells 10 different text books.

--- public_html/public/js/books.js
+++ public_html/public/js/books.js
@@ -1,5 +1,38 @@
-
-
 $(window).on("load", function() {
   loadNavigation(4);
+  loadBooks();
+  $("body").on("click", ".book", (e) => {
+    e.preventDefault(); //don't scroll up
+    openBook(e);
+  });
 });
+
+loadBooks = () => {
+  $.getJSON("/books/textBooks", (books) => {
+    const numBooks = books.length;
+    for (var i = 0; i < numBooks; i++) {
+      let book = books[i];
+      createBookObject(book, "text");
+    }
+  });
+}
+
+createBookObject = (book, bookType) => {
+  $.get("/components/book.html", (bookSkeleton) => {
+    let skeleton = $(bookSkeleton);
+    $("#" + bookType + "-books").append(skeleton);
+    fillData(skeleton, book, bookType);
+  });
+}
+
+fillData = (bookSkeleton, book, bookType) => {
+  let bookTitle = $(bookSkeleton).find(".book-title")[0];
+  $(bookTitle).text(book.fileName);
+  let bookInfo = $(bookSkeleton).find(".book-info")[0];
+  $(bookInfo).text("" + book.fileSize/1024 + " KB");
+  let bookImage = $(bookSkeleton).find("img")[0];
+  $(bookImage).attr("src", "book-" + bookType + ".png");
+}
+openBook = (book) => {
+  window.open("/");
+}
--- public_html/routes/books.js
+++ public_html/routes/books.js
@@ -1,15 +1,38 @@
 const express = require("express");
 const router = express.Router();
+//to get relevant file info
+const fs = require("fs");
 
-//bio router
+//books router
 router.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.sendFile("/books.html", { root: __dirname + "/../public" });
 });
 
-router.get("/books/:bookName", function(req, res){
+router.get("/text/:bookName.txt", function(req, res){
   res.setHeader("Content-Type", "text/html");
-  res.sendFile("/books/:bookName", { root: __dirname + "/../public" });
+  res.sendFile("/books/text/" + req.params.bookName + ".txt", { root: __dirname + "/../public" });
+});
+
+router.get("/textBooks", function(req, res){
+  res.setHeader("Content-Type", "text/json");
+  var directoryPath = "" + __dirname + "/../public/books/text";
+  let dirFiles = [];
+  fs.readdir(directoryPath, (err, files) => {
+    //error handling
+    if (err) {
+        return console.log("Error reading directory: " + err);
+    }
+    //go over every file, recording file size stat
+    files.forEach(function (fileName) {
+        let fileSize = fs.statSync(directoryPath + "/" + fileName).size;
+        let file = {fileName, fileSize};
+        dirFiles.push(file);
+      });
+      res.send(dirFiles);
+      res.end();
+    });
+
 });
 
 module.exports = router;
